2e5c8d582244166fc76b932bdd37509b04b96c8b
Ryo Hirai <m1zuk1lac1e@gmail.com>
2023-08-20 03:50:12 +0900

    Update: streamlit-chatを使ってUIを変更

--- app.py
+++ app.py
@@ -1,42 +1,50 @@
 import g4f
 import streamlit as st
 
-
-def get_gpt3_response(messages: list) -> str:
-    response = g4f.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages,
-    stream=False,
+def generate_g4f_response(messages: list) -> str:
+    response_stream = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True
     )
 
+    response = "".join([message for message in response_stream])
     return response
 
 def main():
-    st.title("GPT4Free Demo")
+    st.title("Chat with GPT4Free!")
 
     # Session state for retaining messages
     if 'messages' not in st.session_state:
-        st.session_state.messages = [{"role": "assistant", "content": "私はAIアシスタントです。何かお手伝いしましょうか？"}]
+        st.session_state.messages = []
 
-    # Placeholder for chat history
-    chat_history_placeholder = st.container()
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(f"{message['content']}")
 
     # Input for the user message
-    user_message = st.text_input("Your Message")
+    user_message = st.chat_input("Your Message")
 
-    # If user has entered a new message
+    # React to user input
     if user_message:
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(f"{user_message}")
+        # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": user_message})
-        gpt3_response = get_gpt3_response(st.session_state.messages)
-        st.session_state.messages.append({"role": "assistant", "content": gpt3_response})
 
-    # Display chat history within the placeholder
-    with chat_history_placeholder :
-        for message in st.session_state.messages:
-            if message["role"] == "user":
-                st.info(f"{message['content']}", icon = "🙂")
-            elif message["role"] == "assistant":
-                st.info(f"{message['content']}", icon = "🤖")
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            for char in generate_g4f_response([{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]):
+                full_response += char
+                message_placeholder.markdown(full_response + "❙")
+
+            message_placeholder.markdown(full_response)
+
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 
 if __name__ == "__main__":
